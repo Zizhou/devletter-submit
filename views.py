cef53835d842f2007d4f8d24f8f42022d5692cdf
Daniel <dan.t.ching@gmail.com>
2014-04-17 19:39:18 -0700

    form is stable, if ugly

--- views.py
+++ views.py
@@ -6,11 +6,56 @@ from submit.models import Developer, Game
 
 def main_page(request):
     developer = Developer.objects.all()
-    
+    message = 'Enter info here:'
     context = {
-	'developer' : developer
+	'developer' : developer,
+	'systemmessage' : message
     }
-    return render(request, 'submit/form.html', context)
 
+    return render(request, 'submit/form.html', context)
 def send(request):
-    return HttpResponse('lol, butts')
+    testtext = 'There is no error.'
+    games = Game.objects.all()
+    devs = Developer.objects.all()
+    halt = False
+
+    for g in games:
+	if request.POST.get('game') == g.name:
+	    halt = True
+	    testtext = 'duplicate game'
+	    continue
+    for d in devs:
+	if request.POST.get('dev-text') == d.name:
+ 	    halt = True
+	    testtext = 'duplicate dev'
+	    continue
+    if request.POST.get('dev-drop') == 'NotADev' and request.POST.get('dev-text') == '':
+	halt = True
+	testtext = 'No dev selected'
+    if request.POST.get('dev-drop') != 'NotADev' and request.POST.get('dev-text') != '':
+	halt = True
+	testtext = 'Two developers'
+    #lazy catch all error
+    #TODO return to form page w/ error
+    if halt == True:
+	testtext = 'Fuck your couch: ' + testtext
+	return render(request, 'submit/form.html', {
+	    'developer': Developer.objects.all(),
+	    'systemmessage' : testtext})
+    else:
+	#create and save the new record
+	r = request.POST #lazy shorthand
+	devname = 'There are FOUR lights'
+	#create new dev record if info entered
+	if request.POST.get('dev-drop') == 'NotADev':
+	    devtemp = Developer(name = r.get('dev-text'), email = r.get('dev-email'), twitter = r.get('dev-twitter'))
+	    devtemp.save()
+	    devname = r.get('dev-text')
+	else:
+	    devname = r.get('dev-drop')
+	#sloppy function to change POST data to boolean
+	lytemp = True if r.get('lastyear') == 'True'  else False
+	#the actual saving is here
+	gametemp =  Game(name = r.get('game'), developer = Developer.objects.get(name = devname), lastyear = lytemp)
+	gametemp.save()
+	return HttpResponse('Muthafuckin\' success!')
